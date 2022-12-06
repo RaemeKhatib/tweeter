5b4c1ef21e5a573d22d2f86b47727125410f8279
Raeme Khatib <raeme.khatib@telus.com>
2022-12-06 16:39:59 -0700

    add a new form for new tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
 });
 
 const onChange = function(e) {
-  //   const input = $(this)
-  // const text = input.val()
+  const input = $(this);
+  const text = input.val();
 
-  const text = $('.input').val();
+  // const text = $('.input').val();
   const remaining = 140 - text.length;
+  const form = input.closest('output');
+  const output = form.fund('output');
 
-  const output = $('.counter');
   output.text(remaining);
   if (remaining < 0) {
     output.addClass('red');
