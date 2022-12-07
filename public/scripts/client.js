f4aeae2796273f5abb3eebd4c20e03aa0d2d2f43
Raeme Khatib <raeme.khatib@telus.com>
2022-12-07 10:49:23 -0700

    we add a section for a sample of other tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,87 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = function(tweetObj) {
+  const name = tweetObj.user.name;
+  const avatar = tweetObj.user.avatars;
+  const handle = tweetObj.user.handle;
+  const text = tweetObj.content.text;
+  const date = tweetObj.created_at; /// will have to come back and format
 
+  const $tweet = $(`<article class="tweet-spot">
+  <header>
+    <div>
+      <img src="${avatar}">
+     <span>${name}</span>
+    </div>
+    <div>
+      <i><strong>${handle}</strong></i>
+    </div>
+  </header>
+  <p>${text}</p>
+  <footer>
+    <hr>
+    <div>
+      <i>${date}</i>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+        <i class="likes">1</i>
+      </div>
+    </div>
+  </footer>
+</article>`);
+  return $tweet;
+};
+
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  $('#tweet-container').append($tweet);// takes return value and appends it to the tweets container
+};
+
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const $tweet = createTweetElement(tweetData);
+
+$(document).ready(function() {
+  renderTweets(data);
+});
